8039f19b3f881e4cfd513f3cb49f8e172005c400
dieterchvatal <dieter.chvatal@gmail.com>
2022-04-28 22:13:30 +0200

    additional diagrams

--- App/tab6.py
+++ App/tab6.py
@@ -33,7 +33,7 @@ def showtecjet(b):
         if not rde.empty:
             rde['datetime'] = pd.to_datetime(rde['starttime'])
             dr2set2 = [
-            {'col':['targetload'],'ylim': [4200, 4800], 'color':'red', 'unit':'kW'},
+            {'col':['maxload','targetload'],'ylim': [4200, 5000], 'color':['FireBrick','red'], 'unit':'kW'},
             {'col':['TJ_GasDiffPressMin'],'ylim': [0, 100], 'color':'orange', 'unit':'mbar'},
             {'col':['TJ_Pos_at_Min'],'ylim': [0, 100], 'color':'purple', 'unit':'°C'},
             {'col':['TJ_GasPress1_at_Min'],'ylim': [700, 1200], 'color':'brown', 'unit':'mbar'},
@@ -45,16 +45,20 @@ def showtecjet(b):
             fig2 = dbokeh_chart(rde, dr2set2, style='both', figsize=dfigsize ,title=ftitle);
             bokeh_show(fig2)
 
+            
+            rde = rde[rde['targetload'] > 0.95 * V.fsm._e['Power_PowerNominal']]
             rde['bmep'] = rde.apply(lambda x: V.fsm._e._calc_BMEP(x['targetload'], V.fsm._e.Speed_nominal), axis=1)
+            rde['bmep2'] = rde.apply(lambda x: V.fsm._e._calc_BMEP(x['maxload'], V.fsm._e.Speed_nominal), axis=1)
             dr2set2 = [
                     #{'col':['targetload'],'ylim': [4100, 4700], 'color':'red', 'unit':'kW'},
-                    {'col':['bmep'],'ylim': [22, 26], 'color':'red', 'unit':'bar'},
+                    {'col':['bmep2','bmep'],'ylim': [20, 30], 'color':['FireBrick','red'], 'unit':'bar'},
                     {'col':['TJ_GasDiffPressMin'],'ylim': [-20, 80], 'color':'blue', 'unit':'mbar'},
                     {'col':['TJ_Pos_at_Min'],'ylim': [0, 600], 'color':'purple', 'unit':'%'},
                     #{'col':['TJ_GasPress1_at_Min'],'ylim': [800, 1300], 'color':'dodgerblue', 'unit':'mbar'},
                     #{'col':['TJ_GasTemp1_at_Min'],'ylim': [0, 100], 'color':'red', 'unit':'°C'},
                     {'col':['no'],'_ylim':(0,1000), 'color':['rgba(0,0,0,0.05)'] },
                     ]
+            dr2set2 = equal_adjust(dr2set2, rde, do_not_adjust=['no'], minfactor=1.0, maxfactor=1.1)
             ntitle = ftitle + ' | BMEP at Start vs TJ Gas Temperature in °C '
             fig3 = dbokeh_chart(rde, dr2set2, x='TJ_GasTemp1_at_Min', style='circle', figsize=dfigsize ,title=ntitle);
             fig3.add_layout(Span(location=24.5,
@@ -63,6 +67,29 @@ def showtecjet(b):
                     dimension='width',x_range_name='default', y_range_name='1',line_color='blue', line_dash='dashed', line_alpha=0.6))
 
             bokeh_show(fig3)
+            
+            dr2set2 = [
+                    #{'col':['targetload'],'ylim': [4100, 4700], 'color':'red', 'unit':'kW'},
+                    {'col':['bmep2','bmep'],'ylim': [20, 30], 'color':['FireBrick','red'], 'unit':'bar'},
+                    #{'col':['TJ_GasDiffPressMin'],'ylim': [-20, 80], 'color':'blue', 'unit':'mbar'},
+                    {'col':['TJ_Pos_at_Min'],'ylim': [0, 600], 'color':'purple', 'unit':'%'},
+                    #{'col':['TJ_GasPress1_at_Min'],'ylim': [800, 1300], 'color':'dodgerblue', 'unit':'mbar'},
+                    {'col':['TJ_GasTemp1_at_Min'],'ylim': [0, 100], 'color':'dodgerblue', 'unit':'°C'},
+                    {'col':['no'],'_ylim':(0,1000), 'color':['rgba(0,0,0,0.05)'] },
+                    ]
+            dr2set2 = equal_adjust(dr2set2, rde, do_not_adjust=['no'], minfactor=1.0, maxfactor=1.1)
+            ntitle = ftitle + ' | BMEP at Start vs TJ TJ_GasDiffPressMin in mbar '
+            fig3 = dbokeh_chart(rde, dr2set2, x='TJ_GasDiffPressMin', style='circle', figsize=dfigsize ,title=ntitle);
+            fig3.add_layout(Span(location=24.5,
+                    dimension='width',x_range_name='default', y_range_name='0',line_color='red', line_dash='dashed', line_alpha=0.6))
+            fig3.add_layout(Span(location=20.0,
+                    dimension='width',x_range_name='default', y_range_name='1',line_color='blue', line_dash='dashed', line_alpha=0.6))
+
+            bokeh_show(fig3)            
+            
+        print()
+        display(rde[V.fsm.results['run2_content']].describe().style.format(precision=2, na_rep='-'))                
+            
         display(rde[V.fsm.results['run2_content']].style.format(precision=2,na_rep='-').hide())
     else:
         print('No Data available.')
@@ -86,7 +113,7 @@ def showtemp(b):
             rde['datetime'] = pd.to_datetime(rde['starttime'])
             dr2set3 = [
                     {'col':['ExhTempCylMax'],'_ylim': [4200, 4800], 'color':'red', 'unit':'°C'},
-                    {'col':['ExhSpread_at_Max'],'_ylim': [0, 100], 'color':'blue', 'unit':'°C'},
+                    {'col':['ExhSpreadMax','ExhSpread_at_Max'],'_ylim': [0, 100], 'color':['dodgerblue','blue'], 'unit':'°C'},
                     {'col':['Power_at_ExhTempCylMax'],'_ylim': [0, 100], 'color':'orange', 'unit':'kW'},
                     {'col':['synchronize'],'_ylim':(-20,400), 'color':'brown', 'unit':'sec'},
                     {'col':['startpreparation'],'_ylim':(-1000,800), 'unit':'sec'},
@@ -98,10 +125,11 @@ def showtemp(b):
             bokeh_show(fig4)
 
             print()
-            display(rde[['no','ExhTempCylMax','ExhSpread_at_Max','Power_at_ExhTempCylMax']].describe()
+            display(rde[['no','ExhTempCylMax','ExhSpreadMax','ExhSpread_at_Max','Power_at_ExhTempCylMax']].describe()
                         .style.format(precision=2, na_rep='-'))            
 
-            display(rde[['no','ExhTempCylMax','ExhSpread_at_Max','Power_at_ExhTempCylMax']]
+            print()
+            display(rde[['no','ExhTempCylMax','ExhSpreadMax','ExhSpread_at_Max','Power_at_ExhTempCylMax']]
                         .style.format(precision=2,na_rep='-').hide())
     else:
         print('No Data available.')
